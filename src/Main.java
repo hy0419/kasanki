094b0050ba0d166bfce9c0952555d42c54b44c0c
yuto <yuto@hiragayuutonoMacBook-Pro.local>
2017-12-22 12:19:39 +0900

    a

--- src/Main.java
+++ src/Main.java
@@ -10,50 +10,51 @@ public class Main {
         System.out.println("二進数で入力してください");
         Scanner scan = new Scanner(System.in);
         String str = scan.next();
-
+        System.out.println(str);
         String[] ArrStr = str.split("");
         int[] ArrInt = new int[ArrStr.length];
         for (int i = 0; i < ArrStr.length; i++) {
             ArrInt[i] = Integer.parseInt(ArrStr[i]);
-
+System.out.println(ArrInt[i]);
         }
         System.out.println(Arrays.toString(ArrInt));
         System.out.println("二進数で入力してください");
         Scanner scan2 = new Scanner(System.in);
         String str2 = scan2.next();
+        System.out.println(str2);
 
-        int input2 = Integer.parseInt(str2);
         String[] ArrStr2 = str2.split("");
         int[] ArrInt2 = new int[ArrStr2.length];
         for (int i = 0; i < ArrStr2.length; i++) {
             ArrInt2[i] = Integer.parseInt(ArrStr2[i]);
+            System.out.println(ArrInt2[i]);
+
         }
-        System.out.println(Arrays.toString(ArrInt2));
 
         Hankasanki hankasanki = new Hankasanki();
         int hankasanresult = hankasanki.HankasankiS(ArrInt[ArrInt.length - 1]
                         , ArrInt2[ArrInt2.length - 1]);//S
         int[] result = new int[str.length() + 1];//答えを入れる配列 入力値＋１
         result[result.length - 1] = hankasanresult;
-        System.out.println(hankasanresult);
-        System.out.println("result.length : "+result.length);
 
         Zenkasanki zenkasanki = new Zenkasanki();
         int c = hankasanki.HankasankiC(ArrInt[ArrInt.length - 1]
                 , ArrInt[ArrInt2.length - 1]);
         int cbox = c;
-        System.out.println("cbox繰り上がり : "+cbox);
-        for(int j = 1; j < str.length();j++){
-            System.out.println("j=" + j);
+        System.out.println("cbox : "+cbox);
+        for(int j = 1; j <= str.length();j++){
+            System.out.println("j : "+j);
+
             result[result.length - j-1] = zenkasanki.
                     zenkasankiS(ArrInt[ArrInt.length - j],ArrInt2[ArrInt2.length -j],cbox);
+            System.out.println("result[result.length - j-1] : "+result[result.length - j-1]);
+            System.out.println("result[j] : "+result[j]);
             cbox = zenkasanki.
                     zenkasankiC(ArrInt[ArrInt.length -j],ArrInt2[ArrInt2.length -j],cbox);
-            System.out.println("result[result.length - j-1] :"+result[result.length - j-1]);
-            System.out.println("ArrInt[ArrInt.length-j]:"+ArrInt[ArrInt.length-j]);
-            System.out.println("cbox繰り上がり :" +cbox+" "+(int)(ArrInt.length-j));
+            System.out.println("cbox : "+cbox);
 
         }
+        System.out.println("str.length() : "+str.length());
 
         System.out.println(Arrays.toString(result));
 
